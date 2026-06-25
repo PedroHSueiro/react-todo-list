f87c782a5b3ab6d44d41327d34effa236ce1c29f
Pedro Henrique <pedsueiro@hotmail.com>
2026-06-25 09:40:50 -0300

    Adding new todo from form to totos list

--- src/components/ToDoForm/index.jsx
+++ src/components/ToDoForm/index.jsx
@@ -3,21 +3,13 @@ import { TextInput } from "../TextInput";
 import "./todo-form.style.css";
 
 export function ToDoForm({ onSubmit }) {
-  const handleFormSubmission = (formData) => {
-    const newItem = {
-      description: formData.get("itemDescription"),
-      completed: false,
-      createdAt: new Date().toLocaleDateString("pt-BR"),
-    };
-    onSubmit(newItem);
-  };
-
   return (
-    <form action={handleFormSubmission} className="todo-form">
+    <form action={onSubmit} className="todo-form">
       <TextInput
         placeholder="Digite o item que deseja adicionar"
         name="itemDescription"
         id="itemDescription"
+        required
       ></TextInput>
       <Button>Salvar item</Button>
     </form>
